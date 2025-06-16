e41b7e8794717ad4c5798d02bde469c128636cce
Hanjocl <hanjo.cl@gmai.com>
2025-06-16 16:07:54 +0100

    FUCK

--- Start_Scripts/start-all.sh
+++ Start_Scripts/start-all.sh
@@ -4,18 +4,12 @@ set -e
 set -x
 
 cd $HOME/Project_RobotSignans/signans_connect
-echo $HOME/Project_RobotSignans/signans_connect
 yarn install
-yarn start &
-
+yarn start > /tmp/yarn.log 2>&1 &
 
 cd $HOME/Project_RobotSignans/signans_python
-echo $HOME/Project_RobotSignans/signans_python
 source .venv/bin/activate
 python main.py > /tmp/main.log 2>&1 &
-echo "Starting back-end"
-
-
 
 # Wait here forever so the script doesn't exit
 wait
